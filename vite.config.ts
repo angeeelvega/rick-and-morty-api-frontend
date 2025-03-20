cb43c24329e79e792855f076f27d9b51dba5d6a9
Ángel Vega <angelantoniovega@gmail.com>
2025-03-20 15:35:55 -0500

    feat: add actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rick-and-morty-api-frontend/',
   resolve: {
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   },
